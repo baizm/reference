a17d4fd93cd2f4faac50b73c46e31a2e26509f43
Marcella Nidiffer <baizm@umich.edu>
2016-05-26 10:48:38 -0400

    Create get_SNP_seqs.py
    This script will extract sequence of 150bp before and after a SNP from a reference assembly given its contig name and position. Input file has on each line a target SNP to extract in the following format: "loc_name contig_name position".

---
+++ get_SNP_seqs.py
@@ -0,0 +1,24 @@
+import subprocess
+import sys
+import glob
+
+#Usage : python get_SNP_seqs.py contig_positions.txt reference.fasta output.fasta
+
+in_file = sys.argv[1]
+reference = sys.argv[2]
+out_file = sys.argv[3]
+
+contigs = []
+
+f=open(in_file, 'r')
+
+for i in f:
+	loc,cont,pos=i.split(' ')
+        start=int(pos)-150
+        end=int(pos)+150
+        contigs.append(cont+':'+str(start)+'-'+str(end))
+
+#create list of contigs to extract
+targets = ' '.join(contigs)
+
+subprocess.call('samtools faidx %s %s > %s' % (reference, targets, out_file), shell=True)
