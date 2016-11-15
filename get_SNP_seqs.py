0c975c6d9a6860f625f3b9ef118a36f47c29bc9b
Marcella Nidiffer <baizm@umich.edu>
2016-11-15 15:43:07 -0500

    Update get_SNP_seqs.py

--- get_SNP_seqs.py
+++ get_SNP_seqs.py
@@ -1,25 +1,28 @@
 import subprocess
 import sys
 
-#Usage : python get_SNP_seqs.py contig_positions.txt reference.fasta output.fasta
+#Usage : python get_SNP_seqs.py contig_positions.txt reference.fasta 250 out.sh
 
 in_file = sys.argv[1]
 reference = sys.argv[2]
-out_file = sys.argv[3]
+size = sys.argv[3]
+out_sh = sys.argv[4]
 
 contigs = []
 
 f=open(in_file, 'r')
+o=open(out_sh, 'w')
 
 for i in f:
 	loc,cont,pos=i.split(' ')
-        start=int(pos)-150
-        end=int(pos)+150
+        start=int(pos)-int(size)
+        end=int(pos)+int(size)
         contigs.append(cont+':'+str(start)+'-'+str(end))
 
 #create list of contigs to extract
 targets = ' '.join(contigs)
 
-subprocess.call('samtools faidx %s %s > %s' % (reference, targets, out_file), shell=True)
+#write command to file, since subprocess won't work (command is too long, have to run in shell)
+o.write('samtools faidx %s %s > all_loci.fasta' % (reference, targets))
 
-#example: python get_SNP_seqs.py ../bgc/lanes12/beta_outliers_contigs.txt AloPal_combined.a.lines.fasta outliers_beta_300bp.fasta
+#out_sh to be run separately
